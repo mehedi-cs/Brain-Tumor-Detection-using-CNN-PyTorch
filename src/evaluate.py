3c42f806144d1512e9d3b72b3b7718ef40224bdb
Md. Mehedi Hasan <mehedicsgo@gmail.com>
2025-10-22 23:07:05 +0600

    Add true_and_pred function for model evaluation

--- src/evaluate.py
+++ src/evaluate.py
@@ -3,3 +3,15 @@ import torch
 from sklearn.metrics import confusion_matrix, classification_report
 import matplotlib.pyplot as plt
 import itertools
+
+def true_and_pred(val_loader, model, device):
+    y_true, y_pred = [], []
+    model.eval()
+    with torch.no_grad():
+        for images, labels in val_loader:
+            images = images.to(device)
+            outputs = model(images)
+            _, preds = torch.max(outputs, 1)
+            y_true.extend(labels.numpy())
+            y_pred.extend(preds.cpu().numpy())
+    return np.array(y_true), np.array(y_pred)
